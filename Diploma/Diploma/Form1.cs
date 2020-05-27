fbbae1037dd1734d5387d94cda1788965e1f3618
Michael Sheetov <michael.shitov@gmail.com>
2020-05-27 20:11:52 +0300

    Diploma v0.2

--- Diploma/Diploma/Form1.cs
+++ Diploma/Diploma/Form1.cs
@@ -1,5 +1,4 @@
-﻿using CenterSpace.NMath.Analysis;
-using CenterSpace.NMath.Core;
+﻿using ScottPlot;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,7 +16,7 @@ namespace Diploma
         double distanceBetweenMasses = 0;
         double firstMass = 0;
         double secondMass = 0;
-        double massRatio;
+        double alpha = 0;
         double L1 = 0, L2 = 0, L3 = 0, L4x = 0, L4y = 0, L5x = 0, L5y = 0;
         public Form1()
         {
@@ -33,36 +32,43 @@ namespace Diploma
 
         private void trackBar1_ValueChanged(object sender, EventArgs e)
         {
-            massRatio = trackBar1.Value;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            firstMass = Convert.ToDouble(textBox1.Text);
+            
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            secondMass = Convert.ToDouble(textBox2.Text);
+            
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            distanceBetweenMasses = Convert.ToDouble(textBox3.Text);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double alpha = secondMass / (firstMass + secondMass);
-            double L1 = distanceBetweenMasses * (1 - Math.Pow((alpha / 3), 1 / 3)); //сделать отдельной функцией, вынести в другой класс
-            double L2 = distanceBetweenMasses * (1 + Math.Pow((alpha / 3), 1 / 3)); //сделать отдельной функцией, вынести в другой класс
+            formsPlot1.Reset();
+            firstMass = Convert.ToDouble(textBox1.Text);
+            secondMass = Convert.ToDouble(textBox2.Text);
+            distanceBetweenMasses = Convert.ToDouble(textBox3.Text);
+            alpha = secondMass / (firstMass + secondMass);
+            double L1 = distanceBetweenMasses * (1 - Math.Pow(alpha / 3, 1.0 / 3.0)); //сделать отдельной функцией, вынести в другой класс
+            double L2 = distanceBetweenMasses * (1 + Math.Pow(alpha / 3, 1.0 / 3.0)); //сделать отдельной функцией, вынести в другой класс
             double L3 = -distanceBetweenMasses * (1 + 5 / 12 * alpha); //сделать отдельной функцией, вынести в другой класс
             double L4x = distanceBetweenMasses / 2 * (firstMass - secondMass) / (firstMass + secondMass); //сделать отдельной функцией, вынести в другой класс
             double L4y = Math.Sqrt(3) / 2 * distanceBetweenMasses; //сделать отдельной функцией, вынести в другой класс
             double L5x = distanceBetweenMasses / 2 * (firstMass - secondMass) / (firstMass + secondMass); //сделать отдельной функцией, вынести в другой класс
             double L5y = -Math.Sqrt(3) / 2 * distanceBetweenMasses; //сделать отдельной функцией, вынести в другой класс
-            double[] dataX =  new double[] { L1, L2, L3, L4x, L5x };
+            double[] dataX = new double[] { L1, L2, L3, L4x, L5x };
             double[] dataY = new double[] { 0, 0, 0, L4y, L5y };
+            double[] dataXFirstMass = new double[] { 0 };
+            double[] dataYFirstMass = new double[] { 0 };
+            double[] dataXSecondMass = new double[] { distanceBetweenMasses };
+            formsPlot1.plt.PlotScatter(dataXFirstMass, dataYFirstMass, markerSize: 15, markerShape: MarkerShape.filledCircle);
+            formsPlot1.plt.PlotScatter(dataXSecondMass, dataYFirstMass, markerSize: 15 * firstMass / secondMass, markerShape: MarkerShape.filledCircle);
             formsPlot1.plt.PlotScatter(dataX, dataY, lineWidth: 0);
             formsPlot1.Render();
         }
