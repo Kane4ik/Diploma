1032c1bde2b353b227aa097b37840ee2c252c0cc
Michael <39634950+Kane4ik@users.noreply.github.com>
2020-06-13 21:43:51 +0300

    finishLine

--- Diploma/Diploma/Form1.Designer.cs
+++ Diploma/Diploma/Form1.Designer.cs
@@ -38,6 +38,12 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,7 +60,6 @@
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(282, 45);
             this.trackBar1.TabIndex = 1;
-            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
             // 
             // label1
             // 
@@ -71,7 +76,6 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -79,7 +83,6 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 5;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label2
             // 
@@ -96,7 +99,6 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 7;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label3
             // 
@@ -117,11 +119,62 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(658, 85);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(506, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "X-ая компонента скорости:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(506, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Y-ая компонента скорости:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(658, 109);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(509, 355);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Запустить таймер";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
@@ -151,6 +204,12 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Diploma/Diploma/Form1.cs
+++ Diploma/Diploma/Form1.cs
@@ -17,7 +17,22 @@ namespace Diploma
         double firstMass = 0;
         double secondMass = 0;
         double alpha = 0;
-        double L1 = 0, L2 = 0, L3 = 0, L4x = 0, L4y = 0, L5x = 0, L5y = 0;
+        double speed0X = 0;
+        double speed0Y = 0;
+        double speedX = 0;
+        double speedY = 0;
+        public const double gravConst = 6.6720e-08;
+        double omega = 0;
+        double initialCoordX;
+        double initialCoordY;
+        double directionCoordX = 0;
+        double directionCoordY = 0;
+        bool plotClicked = true;
+        double centrMass = 0;
+        double accelX = 0; double accelY = 0;
+        double[] particlePathX = new double[100];
+        double[] particlePathY = new double[100];
+        int arrayCounter = 0;
         public Form1()
         {
             InitializeComponent();
@@ -30,47 +45,86 @@ namespace Diploma
             //double[] dataY = new double[] { 1, 4, 9, 16, 25 };
         }
 
-        private void trackBar1_ValueChanged(object sender, EventArgs e)
-        {
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             formsPlot1.Reset();
             firstMass = Convert.ToDouble(textBox1.Text);
             secondMass = Convert.ToDouble(textBox2.Text);
+            speed0X = Convert.ToDouble(textBox4.Text);
+            speed0Y = Convert.ToDouble(textBox5.Text);
             distanceBetweenMasses = Convert.ToDouble(textBox3.Text);
             alpha = secondMass / (firstMass + secondMass);
-            double L1 = distanceBetweenMasses * (1 - Math.Pow(alpha / 3, 1.0 / 3.0)); //сделать отдельной функцией, вынести в другой класс
-            double L2 = distanceBetweenMasses * (1 + Math.Pow(alpha / 3, 1.0 / 3.0)); //сделать отдельной функцией, вынести в другой класс
-            double L3 = -distanceBetweenMasses * (1 + 5 / 12 * alpha); //сделать отдельной функцией, вынести в другой класс
+            omega = gravConst * (firstMass + secondMass) / Math.Pow(distanceBetweenMasses, 2);
+            centrMass = (firstMass + secondMass * distanceBetweenMasses) / (firstMass + secondMass);
+
+            double L1 = distanceBetweenMasses * (1 - Math.Pow(alpha / 3.0, 1.0 / 3)); //сделать отдельной функцией, вынести в другой класс
+            double L2 = distanceBetweenMasses * (1 + Math.Pow(alpha / 3.0, 1.0 / 3)); //сделать отдельной функцией, вынести в другой класс
+            double L3 = -distanceBetweenMasses * (1 + 5.0 / 12 * alpha); //сделать отдельной функцией, вынести в другой класс
             double L4x = distanceBetweenMasses / 2 * (firstMass - secondMass) / (firstMass + secondMass); //сделать отдельной функцией, вынести в другой класс
             double L4y = Math.Sqrt(3) / 2 * distanceBetweenMasses; //сделать отдельной функцией, вынести в другой класс
             double L5x = distanceBetweenMasses / 2 * (firstMass - secondMass) / (firstMass + secondMass); //сделать отдельной функцией, вынести в другой класс
             double L5y = -Math.Sqrt(3) / 2 * distanceBetweenMasses; //сделать отдельной функцией, вынести в другой класс
+
+
+
             double[] dataX = new double[] { L1, L2, L3, L4x, L5x };
             double[] dataY = new double[] { 0, 0, 0, L4y, L5y };
-            double[] dataXFirstMass = new double[] { 0 };
+            double[] dataXFirstMass = new double[] { -secondMass / (firstMass + secondMass) * distanceBetweenMasses };
             double[] dataYFirstMass = new double[] { 0 };
-            double[] dataXSecondMass = new double[] { distanceBetweenMasses };
-            formsPlot1.plt.PlotScatter(dataXFirstMass, dataYFirstMass, markerSize: 15, markerShape: MarkerShape.filledCircle);
-            formsPlot1.plt.PlotScatter(dataXSecondMass, dataYFirstMass, markerSize: 15 * firstMass / secondMass, markerShape: MarkerShape.filledCircle);
+            double[] dataXSecondMass = new double[] { firstMass / (firstMass + secondMass) * distanceBetweenMasses };
+
+            initialCoordX = (firstMass + secondMass * distanceBetweenMasses) / (firstMass + secondMass) - 75;
+            initialCoordY = -32;
+
+            formsPlot1.plt.PlotScatter(dataXFirstMass, dataYFirstMass, markerSize: 10, markerShape: MarkerShape.filledCircle);
+            formsPlot1.plt.PlotScatter(dataXSecondMass, dataYFirstMass, markerSize: 10 * (secondMass / firstMass), markerShape: MarkerShape.filledCircle);
             formsPlot1.plt.PlotScatter(dataX, dataY, lineWidth: 0);
             formsPlot1.Render();
+
+            plotClicked = false;
         }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (arrayCounter < particlePathX.Length)
+            {
+                particlePathX[arrayCounter] = speed0X;
+                particlePathY[arrayCounter] = speed0Y;
+
+                accelX = Math.Round(-gravConst * firstMass * initialCoordX / Math.Pow((Math.Pow(initialCoordX, 2) + Math.Pow((-secondMass / (firstMass + secondMass) * distanceBetweenMasses), 2)), 3.0 / 2.0) - gravConst * secondMass * initialCoordX / Math.Pow((Math.Pow(initialCoordX, 2) + Math.Pow(firstMass / (firstMass + secondMass) * distanceBetweenMasses, 2)), 3.0 / 2.0) + Math.Pow(omega, 2) * initialCoordX + 2 * omega * speed0Y, 15);
+                accelY = Math.Round(-gravConst * firstMass * ((-secondMass / (firstMass + secondMass) * distanceBetweenMasses) - initialCoordY) / Math.Pow(Math.Pow(-secondMass / (firstMass + secondMass) * distanceBetweenMasses - initialCoordY, 2), 3.0 / 2.0) + gravConst * secondMass * (firstMass / (firstMass + secondMass) * distanceBetweenMasses - initialCoordY) / Math.Pow(Math.Pow(firstMass / (firstMass + secondMass) * distanceBetweenMasses - initialCoordY, 2), 3.0 / 2.0) + Math.Pow(omega, 2) * initialCoordY + 2 * omega * speed0X, 15);
+                speed0X = initialCoordX + accelX;
+                speed0Y = initialCoordY + accelY;
+                initialCoordX = particlePathX[arrayCounter];
+                initialCoordY = particlePathY[arrayCounter];
+            }
+            else timer1.Stop();
+            arrayCounter++;
+            formsPlot1.plt.PlotScatter(particlePathX, particlePathY);
+            formsPlot1.Render();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        /*private void formsPlot1_MouseClicked(object sender, MouseEventArgs e)
+        {
+            if (plotClicked == false)
+            {
+                initialCoordX = Cursor.Position.X;
+                initialCoordY = Cursor.Position.Y;
+                
+                double[] dataXClick = new double[] { Cursor.Position.X };
+                double[] dataYClick = new double[] { Cursor.Position.Y };
+                formsPlot1.plt.PlotScatter(dataXClick, dataYClick, markerSize: 15, markerShape: MarkerShape.filledCircle);
+                formsPlot1.Render();
+            }
+            plotClicked = true;
+            
+        }
+        */
     }
 }
